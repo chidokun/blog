e71174f30281f67282829d1bb9bd188410484272
Nguyễn Tuấn <nguyentuanit96@gmail.com>
2020-04-15 22:58:07 +0700

    Update site

--- deploy-site.sh
+++ deploy-site.sh
@@ -3,7 +3,7 @@
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+echo "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Remove the old files in project
 rm -rf public/*
